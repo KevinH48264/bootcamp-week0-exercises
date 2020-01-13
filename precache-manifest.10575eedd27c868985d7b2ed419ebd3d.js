1f0fceb6c0d261ded302854f0abf7a0ec3f1fe75
KevinH48264 <1kevin.huang@gmail.com>
2020-01-13 16:04:16 -0500

    Updates

---
+++ precache-manifest.10575eedd27c868985d7b2ed419ebd3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8243a028a6dcde723836f5622112939f",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/index.html"
+  },
+  {
+    "revision": "93199f82deedf51f7903",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/css/main.c869ab27.chunk.css"
+  },
+  {
+    "revision": "12bd7e79a40b45635aa6",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/2.9c3d41a4.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/2.9c3d41a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93199f82deedf51f7903",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/main.6c4ca8eb.chunk.js"
+  },
+  {
+    "revision": "0fa4e1034d0cce5d7835",
+    "url": "%3CGITHUB_USERNAME%3E.github.io/%3CAPP_NAME%3E/static/js/runtime-main.51614db5.js"
+  }
+]);
